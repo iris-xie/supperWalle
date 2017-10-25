1d27efbdd31b2de523fa43b4f0128898ef08780a
risky-PC\risky <443833189@qq.com>
2017-10-25 23:20:52 +0800

    //test

--- controllers/WalleController.php
+++ controllers/WalleController.php
@@ -507,7 +507,7 @@ class WalleController extends Controller
         file_put_contents('/tmp/xielei.txt',print_r($link_id,true)."11111\n",FILE_APPEND);
 
         $sql = 'SELECT upload_path,file_name FROM project_configuration WHERE project_id = '.$link_id.' ORDER BY updated_at DESC LIMIT 1';
-        $config_path = Configuration::findBySql($sql)->one();
+        $config_path = Configuration::findBySql($sql)->all();
         file_put_contents('/tmp/xielei.txt',print_r($config_path,true)."444\n",FILE_APPEND);
 
         $ret = $revision->copyConfig($this->task,$config_path); // 拷贝配置文件
